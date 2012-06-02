e38b5c7bb22153d703349000c63f0a9805a264aa
Blaise Freeman <blaise@zeeen.com>
2012-06-02 17:13:36 -0400

    first stab at creating states

--- js/app.js
+++ js/app.js
@@ -1,12 +1,47 @@
 // wait for page elements to load
 $(document).ready(function () {
 
-	// init sortable
-	$(function() {
-		$( "#namelist" ).sortable({
-			items: ".sortable"
-		});
-		$( "#namelist" ).disableSelection();
+	// set default state onload
+	$('body').trigger('state', 'view');
+
+	// create state change event
+	$('body').bind('state', function(type){
+		switch(type) {
+			
+			case 'view':
+				$('body').removeClass().addClass("view");
+				console.log('changing state to "view"');
+				break;
+							
+			case 'move':
+				$('body').removeClass().addClass("move");
+				console.log('changing state to "move"');
+				break;
+		}
+	});
+	
+	// create an event that initiates moving a todo item
+	$('.todo').click(function(e){
+		
+		if ($('body').hasClass('move')) {
+			
+			// insert the detached item in this slot
+			$(this).after($selectedToDo);
+			console.log('inserted item');			
+
+			// trigger global state change
+			$('body').trigger('state', 'view');
+
+		} else {
+
+			// remove this todo item but save reference
+			$selectedToDo = $(this).detach();
+
+			// trigger global state change
+			$('body').trigger('state', 'move');
+			
+		}
+		
 	});
 	
 });
