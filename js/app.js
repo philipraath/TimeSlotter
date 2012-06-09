594e5407c27529eb60454eb42a44569ec92de30c
Blaise Freeman <blaise@zeeen.com>
2012-06-09 07:15:57 -0400

    edit/insert actual content

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ $(document).ready(function () {
 
 			case 'view':
 				$('body').removeClass().addClass("view");
+				$("#editbox").appendTo('body').hide();
+				$('.item-body:empty').closest('.todo').remove();
 				console.log('view');
 				break;
 							
@@ -18,6 +20,7 @@ $(document).ready(function () {
 			case 'edit':
 				$('body').removeClass().addClass("edit");
 				$activeTodo.append($("#editbox")).addClass('active');
+				$('#todotext').val($activeTodo.find('.item-body').text());
 				$('#editbox').show();
 				console.log('edit');
 				break;
@@ -29,20 +32,21 @@ $(document).ready(function () {
 
 			case 'view':
 	      // insert a placeholder item
-				$activeTodo = $('<li class="todo"><a data-icon="check" class="item-body" href="#">random item</a><a class="move-btn" data-icon="grid" href="#"></a></li>').insertAfter($(this));
+				$activeTodo = $('<li class="todo"><a data-icon="check" class="item-body" href="#"></a><a class="move-btn" data-icon="grid" href="#"></a></li>').insertAfter($(this));
 				// trigger jquerymobile's listview widget to rewrap all the todo items so that new elements created are rendered with the listview styles/functions/attributes/etc. 
 				$(this).parent().listview('refresh');
-				$('body').trigger('state', 'edit');
+				set_state('edit');
 				break;
 
 			case 'move':
 				// insert the detached item in this slot
 				$activeTodo.insertAfter($(this)).removeClass('popped');
-				$('body').trigger('state', 'view');
+				set_state('view');
 				break;
 
 			case 'edit':
-				break;			
+				set_state('view');
+				break;
 
 		}
 	// bodytext click/tap event
@@ -50,31 +54,38 @@ $(document).ready(function () {
 		switch(get_state()) {
 
 			case 'view':
+				$activeTodo = $(this).closest('.todo');
+				set_state('edit');				
 				break;			
 
 			case 'move':
 				if ($(this).closest('.todo').hasClass('popped')) {
 					// cancel move if clicked on the popped todo
 					$(this).removeClass('popped');
-					$('body').trigger('state', 'view');	
+					set_state('view');	
 				} else {
 					// insert the detached item in this slot
 					$activeTodo.insertAfter($(this).closest('.todo')).removeClass('popped');
-					$('body').trigger('state', 'view');				
+					set_state('view');				
 				}
 				break;
 
 			case 'edit':
+				set_state('view');
 				break;			
 
 		}
 	// move-btn click/tap event
 	}).on('click', '.move-btn', function(e){
 
-		// remove this todo item but save reference
+		// flag todo as in process of being moved
 		$activeTodo = $(this).closest('.todo').addClass('popped');
-		$('body').trigger('state', 'move');			
+		set_state('move');			
 
+	// editbox submission event to update text
+	}).on('updateText', '.todo', function(e){
+		$(this).removeClass('active').find('.item-body').text($("#todotext").val());
+		set_state('view');
 	});
 	
 
@@ -83,25 +94,30 @@ $(document).ready(function () {
 	 * Helper Functions
 	 */
 
-	// helper function to check state of app
+	// helper functions to get/set state of app
 	function get_state() {
 		return $('body').attr('class');
+	}
+	function set_state(state) {
+		$('body').trigger('state', state);
 	}
 
 
 	/*
 	 * Init
 	 */
-	
+
+	// render the editbox with jquerymobile styles manually b/c outside page on load and trigger edit submission
+	$("#editbox").trigger('create').submit(function(e){
+		e.preventDefault();
+		$(this).closest('.todo').trigger('updateText');
+	});
+
 	// set default state onload
-	$('body').trigger('state', 'view');	
+	set_state('view');	
 		
 	// reference to currently active todo item
 	var $activeTodo;
 
-	// render the editbox with jquerymobile styles manually b/c outside page on load
-	$("#editbox").trigger('create').hide().submit(function(e){
-		e.preventDefault();
-	});
 	
 });
