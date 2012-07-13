35ea5c67dfaf8c8fbd2697f6ed2fd28374ee88ca
Philip Raath <philraath@gmail.com>
2012-07-13 11:02:43 -0400

    added app cache check
    to app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,45 @@
 // wait for page elements to load
 $(document).ready(function () {
+	
+	//check app cache status
+	
+	var appCache = window.applicationCache;
+	
+	function handleCacheEvent(e) {
+		console.log('something happened');
+	      }
+	      
+	      function handleCacheError(e) {
+		alert('Error: Cache failed to update!');
+	      };
+	      
+	      
+	      // Fired after the first cache of the manifest.
+	      appCache.addEventListener('cached', handleCacheEvent, false);
+	      
+	      // Checking for an update. Always the first event fired in the sequence.
+	      appCache.addEventListener('checking', handleCacheEvent, false);
+	      
+	      // An update was found. The browser is fetching resources.
+	      appCache.addEventListener('downloading', handleCacheEvent, false);
+	      
+	      // The manifest returns 404 or 410, the download failed,
+	      // or the manifest changed while the download was in progress.
+	      appCache.addEventListener('error', handleCacheError, false);
+	      
+	      // Fired after the first download of the manifest.
+	      appCache.addEventListener('noupdate', handleCacheEvent, false);
+	      
+	      // Fired if the manifest file returns a 404 or 410.
+	      // This results in the application cache being deleted.
+	      appCache.addEventListener('obsolete', handleCacheEvent, false);
+	      
+	      // Fired for each resource listed in the manifest as it is being fetched.
+	      appCache.addEventListener('progress', handleCacheEvent, false);
+	      
+	      // Fired when the manifest resources have been newly redownloaded.
+	      appCache.addEventListener('updateready', handleCacheEvent, false);
+	// end check app cache
 
 	// state change event
 	$('body').bind('state', function(e, type){
