7aa7bfa2354b7113d92e9d45939db02edf2a8bad
Blaise Freeman <blaise@zeeen.com>
2012-08-11 14:47:39 -0400

    fix caching error that broke app...

--- manifest.php
+++ manifest.php
@@ -7,8 +7,9 @@
     $dir = new RecursiveDirectoryIterator(".");
     foreach (new RecursiveIteratorIterator ($dir) as $file){
         if (	$file->IsFile() &&
-            	$file != "./manifest.php" &&
-            	substr($file->getFileName(), 0, 1) != "." &&
+          	  $file->getFileName() != 'cache.manifest' &&
+          	  $file->getFileName() != 'manifest.php' &&
+          	  substr($file->getFileName(), 0, 1) != '.' &&
 							substr($dir, 0, 4) != ".git"  && 
 							substr($dir, 0, 4) != "dev1"  && 
 							substr($dir, 0, 4) != "dev2"  && 
@@ -20,9 +21,5 @@
             $hashes .= md5_file($file);
         }
     }
-    echo "http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.css" . "\n";
-    echo "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" . "\n";
-    echo "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js" . "\n";
-    echo "http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.js" . "\n";
     echo "# Hash: " . md5($hashes) . "\n";
 ?>
